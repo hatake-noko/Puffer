066ba4b5e119ff35bd7053473c51a0622ac24d5c
hatake-noko <hermit183@outlook.com>
2025-03-28 16:49:10 +0900

    Dateクラス群を制作

---
+++ clean/Date.cs
@@ -0,0 +1,20 @@
+using clean;
+
+namespace clean
+{
+    internal class Color
+    {
+    }
+
+    internal class Font
+    {
+    }
+
+    internal class Pos
+    {
+    }
+
+    internal class Time
+    {
+    }
+}
