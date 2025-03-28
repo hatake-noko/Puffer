d8e290ab8c8fa40e1fccfd16e876546cdb6d972f
hatake-noko <hermit183@outlook.com>
2025-03-28 17:13:27 +0900

    Color、Pos、Timeクラスを実装

--- clean/Date.cs
+++ clean/Date.cs
@@ -4,6 +4,15 @@ namespace clean
 {
     internal class Color
     {
+        byte red;
+        byte green;
+        byte blue;
+        byte transparency;
+
+        void SetDefault()
+        {
+            red = green = blue = transparency = 0;
+        }
     }
 
     internal class Font
@@ -12,9 +21,30 @@ namespace clean
 
     internal class Pos
     {
+        int x;
+        int y;
+
+        void SetDefault()
+        {
+            x = y = 0;
+        }
     }
 
     internal class Time
     {
+        int year;
+        int month;
+        int day;
+        int hour;
+        int minute;
+        int second;
+        int millisecond;
+
+        void SetDefault()
+        {
+            year = 2025;
+            month = day = 1;
+            hour = minute = second = millisecond = 0;
+        }
     }
 }
