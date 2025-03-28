2874dca6ba8cc4a90d0789fa8c3d92dfd04f3577
hatake-noko <hermit183@outlook.com>
2025-03-28 17:42:18 +0900

    Infoクラスを実装

--- clean/proc/Info.cs
+++ clean/proc/Info.cs
@@ -4,5 +4,21 @@ namespace clean.proc
 {
     internal class Info
     {
+        int version;
+        byte[] title = new byte[1024];
+        byte[] author = new byte[1024];
+        Time createTime;
+        Time updateTime;
+
+        void SetDefault()
+        {
+            version = 0;
+            title[0] = 0x81;
+            title[1] = 0x00;
+            author[0] = 0x81;
+            author[1] = 0x00;
+            createTime.SetDefault();
+            updateTime.SetDefault();
+        }
     }
 }
