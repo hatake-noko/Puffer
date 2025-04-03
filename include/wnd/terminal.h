efc35a43079a1b4d6c0d86daaf474ca3f86caff6
hatake-noko <hermit183@outlook.com>
2025-04-03 13:38:35 +0900

    create_terminal関数制作

--- include/wnd/terminal.h
+++ include/wnd/terminal.h
@@ -1,4 +1,11 @@
 #ifndef PUFFER_WND_TERMINAL_H
 #define PUFFER_WND_TERMINAL_H
 
+#include <windows.h>
+
+extern HWND terminal_handle;
+extern WNDCLASS terminal_class;
+
+void create_terminal(void);
+
 #endif
