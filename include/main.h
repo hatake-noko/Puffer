2cdedc1950ded571f2e12777a61054b1518e20b0
hatake-noko <hermit183@outlook.com>
2025-04-03 19:27:39 +0900

    グローバル変数の外部参照

--- include/main.h
+++ include/main.h
@@ -3,6 +3,9 @@
 
 #include <windows.h>
 
+extern char cmd[1024][100];
+extern int cmd_size;
+
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow);
 
 #endif
