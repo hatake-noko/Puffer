a27143bfbeb97d7101d61c66ca263474be76de7c
hatake-noko <hermit183@outlook.com>
2025-04-03 20:19:27 +0900

    TerminalWndProc関数宣言

--- include/main.h
+++ include/main.h
@@ -7,5 +7,6 @@ extern char cmd[1024][100];
 extern int cmd_size;
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow);
+LRESULT CALLBACK TerminalWndProc(HWND hwnd, UINT massage, WPARAM wParam, LPARAM lParam);
 
 #endif
