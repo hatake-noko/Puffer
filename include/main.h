ec129dd0d1c39f74efc4023f7aa0cc15716fa9c0
hatake-noko <hermit183@outlook.com>
2025-04-06 09:11:06 +0900

    windowsヘッダを抜去

--- include/main.h
+++ include/main.h
@@ -1,12 +1,9 @@
 #ifndef PUFFER_MAIN_H
 #define PUFFER_MAIN_H
 
-#include <windows.h>
-
 extern char cmd[1024][100];
 extern int cmd_size;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, int nCmdShow);
-LRESULT CALLBACK TerminalWndProc(HWND hwnd, UINT massage, WPARAM wParam, LPARAM lParam);
+int main(void);
 
 #endif
