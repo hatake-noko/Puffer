8af07eb226d98c6e7da0927f1bd2ecf5eed18195
hatake-noko <hermit183@outlook.com>
2025-04-06 14:19:34 +0900

    main関数の安全性強化

--- src/main.c
+++ src/main.c
@@ -5,30 +5,42 @@
 #include <string.h>
 
 char cmd[100][1024];
-int cmdSize;
+int cmd_size;
 char select[10][100];
-int selectSize;
+int select_size;
 
 int main(void){
     strcpy(cmd[0], "select puffer");
-    cmdSize = 1;
+    cmd_size = 1;
     strcpy(select[0], "puffer");
-    selectSize = 1;
+    select_size = 1;
 
     while(1){
-        for(int i = 0; i < cmdSize; i ++){
-            for(int j = 0; j < selectSize; j ++){
+        for(int i = 0; i < cmd_size; i ++){
+            for(int j = 0; j < select_size; j ++){
                 printf("%s: ", select[j]);
             }
             printf("%s\n", cmd[i]);
         }
-        scanf("%s", cmd[cmdSize]);
-        cmdSize ++;
+        for(int i = 0; 1; i ++){
+            int c = getchar();
+            if(c == EOF){
+                fputs("入力中にエラーが発生した可能性があります\n", stderr);
+            }else if(c == '\n'){
+                cmd[cmd_size][i] = '\0';
+                break;
+            }else{
+                cmd[cmd_size][i] = (char)c;
+            }
+        }
+        cmd_size ++;
 
-        if(strcmp(cmd[cmdSize - 1], "unselect")){
+        if(strcmp(cmd[cmd_size - 1], "unselect")){
             break;
-        }else if(strcmp(cmd[cmdSize - 1], "version")){
+        }else if(strcmp(cmd[cmd_size - 1], "version")){
             printf("version: 1.0");
         }
     }
+
+    return 0;
 }
