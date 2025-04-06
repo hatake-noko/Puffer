92262e0b4909bbb88cb0285ef70283da5fc2346b
hatake-noko <hermit183@outlook.com>
2025-04-06 09:30:18 +0900

    cmdの標準出入力とその処理

--- src/main.c
+++ src/main.c
@@ -5,9 +5,30 @@
 #include <string.h>
 
 char cmd[100][1024];
-int cmdSize = 0;
+int cmdSize;
 char select[10][100];
-int selectSize = 0;
+int selectSize;
 
 int main(void){
+    strcpy(cmd[0], "select puffer");
+    cmdSize = 1;
+    strcpy(select[0], "puffer");
+    selectSize = 1;
+
+    while(1){
+        for(int i = 0; i < cmdSize; i ++){
+            for(int j = 0; j < selectSize; j ++){
+                printf("%s: ", select[j]);
+            }
+            printf("%s\n", cmd[i]);
+        }
+        scanf("%s", cmd[cmdSize]);
+        cmdSize ++;
+
+        if(strcmp(cmd[cmdSize - 1], "unselect")){
+            break;
+        }else if(strcmp(cmd[cmdSize - 1], "version")){
+            printf("version: 1.0");
+        }
+    }
 }
