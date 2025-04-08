80edca18285fb2578ef672ff0846c0014fb2d0a1
hatake-noko <hermit183@outlook.com>
2025-04-08 18:27:15 +0900

    print_info関数実装

--- src/info.c
+++ src/info.c
@@ -1,5 +1,6 @@
 #include "info.h"
 
+#include <stdio.h>
 #include <string.h>
 #include "proc.h"
 
@@ -8,5 +9,12 @@ void init_info(info_t *info){
     info->version = 1;
     strcpy(info->title, "default title");
     strcpy(info->author, "default author");
+    return;
+}
+
+void print_info(const info_t *info){
+    printf("version: %d\n", info->version);
+    printf("title: %s\n", info->title);
+    printf("author: %s\n", info->author);
     return;
 }
