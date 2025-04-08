28d563ee5e39a4c64e4da3a869615fcc8ed98e8a
hatake-noko <hermit183@outlook.com>
2025-04-08 16:10:10 +0900

    init_puffer関数実装のバグ修正

--- src/puffer.c
+++ src/puffer.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "info.h"
 
-void init_puffer(puffer_t puffer){
+void init_puffer(puffer_t *puffer){
     strcpy(puffer->path_dir, "(c:\\");
     strcpy(puffer->path_name, "untitled");
     init_info(&(puffer->info));
