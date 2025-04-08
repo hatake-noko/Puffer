928d0d5cec732ae1bed87d7372dd26760f804e14
hatake-noko <hermit183@outlook.com>
2025-04-08 16:13:33 +0900

    init_info関数にversion初期化を実装

--- src/info.c
+++ src/info.c
@@ -5,6 +5,7 @@
 
 void init_info(info_t *info){
     init_proc(&(info->tag));
+    info->version = 1;
     strcpy(info->title, "default title");
     strcpy(info->author, "default author");
     return;
