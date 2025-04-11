7e84bc6669343a0a7f8db41a14b62453755f5f2f
hatake-noko <hermit183@outlook.com>
2025-04-11 18:23:22 +0900

    CleanMsg関数宣言

--- include/main.h
+++ include/main.h
@@ -1,11 +1,12 @@
 #ifndef PUFFER_MAIN_H
 #define PUFFER_MAIN_H
 
-#include <windows.h>
+#include<windows.h>
 
 extern char cmd[1024][100];
 extern int cmd_size;
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
+int WINAPI WinMain(HINSTANCE hInstance,HINSTANCE hPrevInstance,LPSTR lpCmdLine,int nCmdShow);
+LRESULT CALLBACK CleanMsg(HWND h,UINT m,WPARAM w,LPARAM l);
 
 #endif
