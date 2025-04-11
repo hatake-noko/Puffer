8d9f0f446033bc13242e8ffb8bf4edb7c7a63c71
hatake-noko <hermit183@outlook.com>
2025-04-11 17:43:38 +0900

    windowsヘッダの挿入

--- include/main.h
+++ include/main.h
@@ -1,9 +1,11 @@
 #ifndef PUFFER_MAIN_H
 #define PUFFER_MAIN_H
 
+#include <windows.h>
+
 extern char cmd[1024][100];
 extern int cmd_size;
 
-int main(void);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow);
 
 #endif
