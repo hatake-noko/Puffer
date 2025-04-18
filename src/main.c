6db2126505c2a4b7771c16b807101ad90114c652
hatake-noko <hermit183@outlook.com>
2025-04-18 20:22:41 +0900

    main関数ni

--- src/main.c
+++ src/main.c
@@ -7,6 +7,7 @@ int main(void){
     char cmd[1024];
     char select[10][255];
     int select_size;
+    char token[1024];
 
     strcpy(select[0], "Puffer");
     select_size = 1;
@@ -18,19 +19,22 @@ int main(void){
         printf(" ? ");
         gets(cmd);
 
+        if(get_nth_token(token, cmd, 1) == EXIT_FAILURE){
+            continue;
+        }
         switch(select_size){
         case 0:
-            if(strcmp(cmd, "ver") == 0){
+            if(strcmp(token, "ver") == 0){
                 puts("compatible version: 1");
-            }else if(strcmp(cmd, "unselect") == 0){
+            }else if(strcmp(token, "unselect") == 0){
                 return 0;
             }else{
                 goto no_find;
             }
             break;
         default:
-        no_find:
-            printf("error: no find the cmd `%s'", cmd);
+no_find:
+            printf("error: no find the cmd `%s\'", cmd);
             break;
         }
     }
