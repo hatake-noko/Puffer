f28450311b56ba9f6fb4e77d4cc478506c0311c8
hatake-noko <hermit183@outlook.com>
2025-04-20 10:41:55 +0900

    set_page構造体制作

---
+++ include/set_page.h
@@ -0,0 +1,12 @@
+#ifndef PUFFER_SET_PAGE_H
+#define PUFFER_SET_PAGE_H
+
+#include "literal.h"
+#include "obj.h"
+
+typedef struct tag_set_page_s{
+    num_l obj_size;
+    obj_s *obj;
+}set_page_s;
+
+#endif
