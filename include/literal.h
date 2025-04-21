d70032313aaea4e74e96a859af1b2907bf58200a
hatake-noko <hermit183@outlook.com>
2025-04-21 14:53:20 +0900

    read_color関数宣言

--- include/literal.h
+++ include/literal.h
@@ -7,11 +7,12 @@
 typedef uint8_t byte_l;
 typedef uint32_t num_l;
 typedef uint8_t str_l[1024];
-typedef uint16_t color_l;
+typedef uint32_t color_l;
 typedef uint64_t pos_l;
 
 void read_byte(byte_l *byte, FILE *file);
 void read_num(num_l *num, FILE *file);
 void read_str(str_l str, FILE *file);
+void read_color(color_l *color, FILE *file);
 
 #endif
