d7ef3d8f73be9d635ecdccaf023c2fe21e483c37
hatake-noko <hermit183@outlook.com>
2025-04-21 10:37:43 +0900

    read_byte関数宣言

--- include/literal.h
+++ include/literal.h
@@ -2,11 +2,14 @@
 #define PUFFER_LITERAL_H
 
 #include <stdint.h>
+#include <stdio.h>
 
 typedef uint8_t byte_l;
 typedef uint32_t num_l;
 typedef uint8_t str_l[1024];
 typedef uint16_t color_l;
 typedef uint64_t pos_l;
+
+void read_byte(byte_l *byte, FILE *file);
 
 #endif
