b96cab422f8d245b5c8fe4f8e49747435e4aaa73
hatake-noko <hermit183@outlook.com>
2025-04-21 12:56:09 +0900

    read_num関数実装

--- src/literal.c
+++ src/literal.c
@@ -6,3 +6,26 @@ void read_byte(byte_l *byte, FILE *file){
     fread(byte, sizeof(*byte), 1, file);
     return;
 }
+
+void read_num(num_l *num, FILE *file){
+    byte_l size;
+    byte_l byte;
+    num_l digit;
+
+    fread(&size, sizeof(size), 1, file);
+    digit = 0x100'00;
+    for(int i = 1; i < size / 0x10 || i < 2; i ++){
+        digit *= 0x100;
+    }
+    for(int i = 0; i < size / 0x10 || i < 2; i ++){
+        fread(&byte, sizeof(byte), 1, file);
+        *num += byte * digit;
+        digit /= 0x100;
+    }
+    for(int i = 0; i < size % 0x10 || i < 2; i ++){
+        fread(&byte, sizeof(byte), 1, file);
+        *num += byte * digit;
+        digit /= 0x100;
+    }
+    return;
+}
