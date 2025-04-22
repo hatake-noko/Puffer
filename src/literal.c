6b1b754c1bf127fb5fba6491713f95ac68ed2cc4
hatake-noko <hermit183@outlook.com>
2025-04-22 12:33:46 +0900

    read_color関数実装

--- src/literal.c
+++ src/literal.c
@@ -40,3 +40,43 @@ void read_str(str_l str, FILE *file){
     str[1023] = '\0';
     return;
 }
+
+void read_color(color_l *color, FILE *file){
+    byte_l mode;
+    byte_l byte;
+
+    fread(&mode, sizeof(mode), 1, file);
+    *color = 0x00'00'00'00;
+    switch(mode){
+    case 0x00:
+        fread(&byte, sizeof(byte), 1, file);
+        *color += byte * 0x100'00'00;
+    case 0x01:
+        fread(&byte, sizeof(byte), 1, file);
+        *color += byte * 0x100'00;
+        fread(&byte, sizeof(byte), 1, file);
+        *color += byte * 0x100;
+        fread(&byte, sizeof(byte), 1, file);
+        *color += byte * 0x1;
+        break;
+    case 0x10:
+        *color = 0xffffff;
+        break;
+    case 0x11:
+        *color = 0x000000;
+        break;
+    case 0x12:
+        *color = 0xff3a2f;
+        break;
+    case 0x13:
+        *color = 0x33c759;
+        break;
+    case 0x14:
+        *color = 0x0061fd;
+        break;
+    case 0x15:
+        *color = 0xffcc01;
+        break;
+    }
+    return;
+}
