d0ab1afc1003661d02206a0c090b1dfdcec3a774
hatake-noko <hermit183@outlook.com>
2025-04-23 16:49:42 +0900

    set_pageソースのバグ修正

--- src/set_page.c
+++ src/set_page.c
@@ -1,7 +1,8 @@
 #include "set_page.h"
 
 void init_set_page(set_page_s *set_page){
-    set_page.obj_size = 0;
-    set_page.obj = NULL;
+    init_set(&set_page->set);
+    set_page->obj_size = 0;
+    set_page->obj = NULL;
     return;
 }
