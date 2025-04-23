c480a189c80142d5ac4374c08090727e5178b4f0
hatake-noko <hermit183@outlook.com>
2025-04-23 16:45:28 +0900

    set_infoソースのバグ修正

--- src/set_info.c
+++ src/set_info.c
@@ -4,8 +4,8 @@
 
 void init_set_info(set_info_s *set_info){
     init_set(&(set_info->set));
-    ver = 0x00010000;
-    author[0] = title[0] = 0x20;
-    author[1] = title[1] = 0x00;
+    set_info->ver = 0x00010000;
+    set_info->author[0] = set_info->title[0] = 0x20;
+    set_info->author[1] = set_info->title[1] = 0x00;
     return;
 }
