7b66ed63d489c3be16eabf48e1f649dc730f5a9d
hatake-noko <hermit183@outlook.com>
2025-04-30 13:57:23 +0900

    byteリテラル（型変数）をnum1リテラル（構造体）に変更

--- include/literal.h
+++ include/literal.h
@@ -4,13 +4,15 @@
 #include <stdint.h>
 #include <stdio.h>
 
-typedef uint8_t byte_l;
+typedef struct tag_num1_l{
+    uint8_t content;
+}num1_l;
 typedef uint32_t num_l;
 typedef uint8_t str_l[1024];
 typedef uint32_t color_l;
 typedef uint64_t pos_l;
 
-void read_byte(byte_l *byte, FILE *file);
+void read_num1(num1_l *num1, FILE *file);
 void read_num(num_l *num, FILE *file);
 void read_str(str_l str, FILE *file);
 void read_color(color_l *color, FILE *file);
