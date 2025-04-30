7f6ff6f5dc403730940f49978ce4c1136284c448
hatake-noko <hermit183@outlook.com>
2025-04-30 14:00:35 +0900

    strリテラル（型変数）をstr1024リテラル（構造体）に変更

--- include/literal.h
+++ include/literal.h
@@ -10,13 +10,15 @@ typedef struct tag_num1_l{
 typedef struct tag_num4_l{
     uint8_t content;
 }num4_l;
-typedef uint8_t str_l[1024];
+typedef typedef struct tag_str1024_l{
+    uint8_t content[1024];
+}str1024_l;
 typedef uint32_t color_l;
 typedef uint64_t pos_l;
 
 void read_num1(num1_l *num1, FILE *file);
 void read_num4(num4_l *num4, FILE *file);
-void read_str(str_l str, FILE *file);
+void read_str1024(str1024_l str1024, FILE *file);
 void read_color(color_l *color, FILE *file);
 void read_pos(pos_l *pos, FILE *file);
 
