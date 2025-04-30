0aad74f8aa8900232b45113d1d5580a11f9232fc
hatake-noko <hermit183@outlook.com>
2025-04-30 13:58:37 +0900

    numリテラル（型変数）をnum4リテラル（構造体）に変更

--- include/literal.h
+++ include/literal.h
@@ -7,13 +7,15 @@
 typedef struct tag_num1_l{
     uint8_t content;
 }num1_l;
-typedef uint32_t num_l;
+typedef struct tag_num4_l{
+    uint8_t content;
+}num4_l;
 typedef uint8_t str_l[1024];
 typedef uint32_t color_l;
 typedef uint64_t pos_l;
 
 void read_num1(num1_l *num1, FILE *file);
-void read_num(num_l *num, FILE *file);
+void read_num4(num4_l *num4, FILE *file);
 void read_str(str_l str, FILE *file);
 void read_color(color_l *color, FILE *file);
 void read_pos(pos_l *pos, FILE *file);
